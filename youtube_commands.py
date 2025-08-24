09e6404df7223c1d8d95c106cf6162017510fbf5
YamanakaKazuo <damirmukanov8@gmail.com>
2025-08-24 23:29:22 +0500

    [FEATURE] Add youtube commands and downloader

---
+++ youtube_commands.py
@@ -0,0 +1,85 @@
+from aiogram import Router, F, Bot
+from aiogram.filters.command import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, FSInputFile
+
+from youtube_downloader import YoutubeDownloader
+from telethon_sender import send_video_telethon
+
+router = Router(name=__name__)
+
+resolutions = {
+    144: 256,
+    240: 320,
+    360: 640,
+    480: 640,
+    720: 1280,
+    1080: 1920,
+    1440: 2560,
+    2160: 3840,
+    4320: 7680
+}
+
+
+class YoutubeForm(StatesGroup):
+    video_url = State()
+
+
+@router.message(Command("youtube"))
+async def youtube_start(message: Message, state: FSMContext):
+    await state.set_state(YoutubeForm.video_url)
+    await message.answer('🔗 Send me the link to your video!')
+
+
+@router.message(YoutubeForm.video_url)
+async def youtube_url(message: Message, state: FSMContext):
+    await state.clear()
+    url = message.text
+    try:
+        yd = YoutubeDownloader(url)
+        video_data = yd.get_video_info()
+        video_data['formats'] = sorted([el for el in set(video_data['formats']) if el in [144, 360, 480, 720, 1080, 1440, 2160]])
+
+        res = []
+        line = []
+        for i, format in enumerate(video_data['formats']):
+            if i % 2 == 0:
+                line = [InlineKeyboardButton(text=f"{format}p", callback_data=f"D|{url}|{format}|{message.from_user.id}"),]
+                if i == len(video_data['formats']) - 1:
+                    res.append(line)
+            else:
+                line.append(InlineKeyboardButton(text=f"{format}p", callback_data=f"D|{url}|{format}|{message.from_user.id}"))
+                res.append(line)
+                line = []
+        keyboard = InlineKeyboardMarkup(inline_keyboard=res)
+        await message.answer_photo(photo=video_data["thumbnail"], caption=f"📹 <b>{video_data['title']}</b>\n👤 {video_data['uploader']}\n\n<b>Download formats ↓</b>", reply_markup=keyboard)
+
+    except Exception as e:
+        await message.answer('Wrong url, bro :(')
+        print(e)
+
+
+@router.callback_query(F.data.startswith("D"))
+async def download_video_by_callback(query: CallbackQuery, bot: Bot):
+    elements = query.data.split("|")
+    await bot.send_message(chat_id=elements[3], text=f'⏰ Downloading, please wait...')
+    yd = YoutubeDownloader(elements[1])
+    file_data = yd.download_video_by_resolution(elements[2])
+    h = int(elements[2])
+    w = resolutions[h]
+    await send_video_telethon(query.from_user.id, f"{file_data['title']}.{file_data['ext']}", w, h, file_data['duration'])
+
+
+@router.message(F.video)
+async def youtube_video_get(message: Message, bot: Bot):
+    if message.from_user.id == 863400079 and message.caption:
+        data = message.caption.split(";")
+        await bot.send_video(
+            chat_id=data[0],
+            video=message.video.file_id,
+            supports_streaming=True,
+            width=int(data[2]),
+            height=int(data[3]),
+            duration=int(data[1])
+        )
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -3,6 +3,7 @@ from yt_dlp import YoutubeDL
 url = "https://www.youtube.com/watch?v=ZHfS8Q8-OtQ"
 # url = "https://www.youtube.com/watch?v=R3GfuzLMPkA"
 
+
 class YoutubeDownloader:
     def __init__(self, url: str):
         self.url = url
@@ -31,32 +32,19 @@ class YoutubeDownloader:
             'format': f'bv*[height<={resolution}]+ba/best',
             'outtmpl': '%(title)s.%(ext)s',
             'ffmpeg_location': r"H:\ffmpeg",
+            'merge_output_format': 'mp4',
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4',
+            }],
         }
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([self.url])
             info = ydl.extract_info(self.url, download=False, process=False)
             title = info.get("title")
-            ext = info.get("ext")
+            duration = info.get("duration")
             return {
                 "title": title,
-                "ext": ext,
+                "ext": "mp4",
+                "duration": duration,
             }
-
-# print(YoutubeDownloader(url).get_video_info())
-
-# ydl_opts = {
-#     'format': 'bv*[height<=720]+ba/best',
-#     'outtmpl': '%(title)s.%(ext)s',
-#     'ffmpeg_location': r"H:\ffmpeg",
-# }
-# with YoutubeDL(ydl_opts) as ydl:
-#     info = ydl.extract_info(url, download=False)
-#     print("Title:", info.get("title"))
-#     print("Thumbnail:", info.get("thumbnail"))
-#     print("Uploader:", info.get("uploader"))
-#     print("Duration:", info.get("duration"))
-#     print("\nAvailable formats:")
-#     for f in info.get("formats", []):
-#         print(f"{f['format_id']} - {f.get('ext')} - {f.get('height')}p - {f.get('filesize', 'unknown')} bytes")
-#
-#     ydl.download([url])
